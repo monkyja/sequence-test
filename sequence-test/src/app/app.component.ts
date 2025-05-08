a8e22a5c9c19b4ef8a12b40ba9584f6ffd785884
Juan Antonio Ocaña <monkyja@gmail.com>
2025-05-08 18:01:10 +0200

    Refactor imports in AppComponent and update tsconfig paths for better module resolution

--- sequence-test/src/app/app.component.ts
+++ sequence-test/src/app/app.component.ts
@@ -1,13 +1,9 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { HeaderComponent } from "./components/header/header.component";
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from "@components/header/header.component";
 
 @Component({
   selector: 'app-root',
   imports: [HeaderComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-  ngOnInit() {
-
-  }
-}
+export class AppComponent {}
