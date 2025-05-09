cda00d79444905778647ef7511130ea4ad4a9a43
Juan Antonio Ocaña <monkyja@gmail.com>
2025-05-09 17:54:38 +0200

    Update menu routerLink

--- sequence-test/src/app/components/menu/menu.component.ts
+++ sequence-test/src/app/components/menu/menu.component.ts
@@ -4,12 +4,22 @@ import { TranslocoPipe } from '@jsverse/transloco';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { RouterLink } from '@angular/router';
+import { ROUTES } from '@const/routes';
 
 @Component({
   selector: 'app-menu',
-  imports: [NzLayoutModule, NzMenuModule, NzIconModule, TranslocoPipe],
+  imports: [
+    NzLayoutModule,
+    NzMenuModule,
+    NzIconModule,
+    TranslocoPipe,
+    RouterLink
+  ],
   templateUrl: './menu.component.html',
 })
 export class MenuComponent {
   appStore = inject(AppStore);
+
+  ROUTES = ROUTES;
 }
