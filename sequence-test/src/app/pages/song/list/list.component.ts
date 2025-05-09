09bc9fd1b0c44c705f8d8a56cf5e57da060783b0
Juan Antonio Ocaña <monkyja@gmail.com>
2025-05-09 12:35:17 +0200

    create list component page

--- sequence-test/src/app/pages/song/list/list.component.ts
+++ sequence-test/src/app/pages/song/list/list.component.ts
@@ -1,13 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { AppStore } from '@app/app.store';
+import { SongStore } from '@stores/song/song.store';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
 
 @Component({
-  selector: 'app-list',
-  imports: [],
+  imports: [NzGridModule, NzListModule, NzSpinModule, NzEmptyModule],
   templateUrl: './list.component.html',
 })
 export class SongListComponent {
   appStore = inject(AppStore);
+  songStore = inject(SongStore);
+
+  songs = this.songStore.getSongs();
 
   constructor() {
     this.appStore.setTitle('pages.song.list.pageTitle');
